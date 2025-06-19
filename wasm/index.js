b4c3f334e7504085710ccaf0014a5dc32548d094
prnvbn <prnvbn@gmail.com>
2025-06-19 11:36:41 +0100

    UPDATE theme toggle to persist between loads

--- wasm/index.js
+++
@@ -1,6 +0,0 @@
-// Theme toggle functionality
-const themeToggle = document.getElementById("theme-toggle");
-
-themeToggle.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark");
-});
